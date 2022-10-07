a2213c28a5f8d98e4219e4acd51d08d5d9242745
LookerKy <nagaraz07@gmail.com>
2022-10-07 14:43:45 +0900

    prettierrc added

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -1,11 +1,8 @@
-import React from 'react'
-import {Text} from '@mantine/core'
-
+import React from 'react';
+import { Text } from '@mantine/core';
 
 const Settings = () => {
-    return (<Text>Settings Component</Text>)
-
-
-}
+  return <Text>Settings Component</Text>;
+};
 
 export default Settings;
